c363b9f472091cdc5566fb1e27b5f896e549ae5c
wandersonalwes <wandersonalwes@hotmail.com>
2020-08-28 20:29:18 -0300

    edit categories

--- pages/categorias/[id].js
+++ pages/categorias/[id].js
@@ -1,54 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Layout, ProductGrid, ProductItem } from '../../components';
-import { useRouter } from 'next/router';
-import api from '../../services/api';
 
-const Category = () => {
-  const [category, setCategory] = useState([]);
-  const router = useRouter();
-  const [loading, setLoading] = useState(false);
-  const { id } = router.query;
-
-  useEffect(() => {
-
-    if (id !== undefined) {
-      api.get(`/categories/${id}`).then(response => {
-        setCategory(response.data);
-        setLoading(true);
-      });
-    }
-  }, [id]);
+const Category = ({ category }) => {
 
   return (
     <Layout>
-
-      {loading === true ? (
-        <div className="mb">
-          <h1 className="mb">{category.name}</h1>
-          {category.products && category.products.length > 0 ? (
-            <ProductGrid>
-              {category.products.map(product => {
-                return (
-                  <div key={product.id}>
-                    <ProductItem
-                      id={product.id}
-                      name={product.name}
-                      price={product.price}
-                      photoURL={product.photos[0].url}
-                    />
-                  </div>
-                )
-              })}
-            </ProductGrid>
-          ) : <h2>Nenhum produto encontrado!</h2>}
-        </div>
-
-      ) : (
-          <h2>Carregando...</h2>
-        )}
-
+      <div className="mb">
+        <h1 className="mb">{category.name}</h1>
+        {category.products.length > 0 ? (
+          <ProductGrid>
+            {category.products.map(product => {
+              return (
+                <div key={product.id}>
+                  <ProductItem
+                    id={product.id}
+                    name={product.name}
+                    price={product.price}
+                    photoURL={product.photos[0].url}
+                  />
+                </div>
+              )
+            })}
+          </ProductGrid>
+        ) : (
+            <h2>Nenhum produto encontrado</h2>
+          )}
+      </div>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/categories`)
+  const categories = await res.json()
+
+  const paths = categories.map((product) => ({
+    params: { id: product.id.toString() },
+  }))
+
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+  const { NEXT_PUBLIC_API_URL } = process.env
+
+  const res = await fetch(`${NEXT_PUBLIC_API_URL}/categories/${params.id}`)
+
+  const category = await res.json()
+
+  return { props: { category } }
 }
 
 export default Category;
